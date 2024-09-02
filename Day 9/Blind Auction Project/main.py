ce97a68204964c3b64012f4f37333c90f8f6ac43
Nils Goovaerts <nils.goovaerts@euri.com>
2024-09-02 15:16:31 +0200

    finish auction

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,29 @@
 # TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+import \
+    os
 
+moreInputRequired = True
+bids = {}
+while moreInputRequired:
+    name = input("Your name:\n")
+    price = float(input("Your price:\n"))
+    # TODO-2: Save data into dictionary {name: price}
+    bids[name] = price
+    
+    # TODO-3: Whether if new bids need to be added
+    moreBids = input("More bids? y?").lower()
+    if moreBids != 'y':
+        moreInputRequired = False
+    
+    os.system("clear")
+# TODO-4: Compare bids in dictionary
+highestBidBy = ''
+for bidBy in bids:
+    if highestBidBy == '':
+        highestBidBy = bidBy
+    else:
+        if bids[bidBy] > bids[highestBidBy]:
+            highestBidBy = bidBy
+
+print(f'Highest bid of {bids[highestBidBy]} by {highestBidBy}')
 

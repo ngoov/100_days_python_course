a986c50f52ee0acd44320a82725bf4d3f815552e
Nils Goovaerts <nils.goovaerts@euri.com>
2024-09-02 13:56:16 +0200

    ceasar part 2

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -6,10 +6,16 @@ shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+def decrypt(cipher_text, shift_amount):
+    original_text = ""
+    for letter in cipher_text:
+        original_position = alphabet.index(letter) - shift_amount
+        original_position %= len(alphabet)
+        original_text += alphabet[original_position]
+    print(f"Here is the decoded result: {original_text}")
+
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
 def encrypt(original_text, shift_amount):
     cipher_text = ""
@@ -19,8 +25,17 @@ def encrypt(original_text, shift_amount):
         cipher_text += alphabet[shifted_position]
     print(f"Here is the encoded result: {cipher_text}")
 
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+def ceasar(text, shift_amount, direction):
+    if direction == 'encode':
+        encrypt(text, shift_amount)
+    elif direction == 'decode':
+        decrypt(text, shift_amount)
+    else:
+        print('Invalid action')
 
-encrypt(original_text=text, shift_amount=shift)
+ceasar(text=text, shift_amount=shift, direction=direction)
 
 
 

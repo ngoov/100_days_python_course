c47c2a6a14fbf74bc84c50b38cc94576a090ee07
Nils Goovaerts <nils.goovaerts@euri.com>
2025-06-20 12:15:02 +0200

    add day-24

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -6,6 +6,8 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -14,16 +16,14 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
     def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
